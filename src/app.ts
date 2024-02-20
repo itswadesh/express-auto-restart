ffe756acc753ade21cb478d0a41df26841d5d19b
ritikranjan12 <ritik123453@gmail.com>
2024-02-20 14:01:29 +0530

    customized product added

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,8 @@ import { getCollection } from "./db"
 
 export const x = 1001222200
 
-export const FetchPrice = async({args}) => {
+//TODO: 
+export const FetchPrice = async({args}: {args: any}) => {
     
     const {STYLE, SIZE,STONE_TYPE,Metal_Type, word} = args
 
@@ -12,8 +13,21 @@ export const FetchPrice = async({args}) => {
     const productsorders = await productsCollection
       .find({ STYLE, SIZE, STONE_TYPE, Metal_Type, Letter: { $in: letterArray }})
       .toArray()
-    for (const p of productsorders) {
-        total += parseInt(p.Component_Price)
+    const letterMap = new Map(); 
+    for (const letter of letterArray) 
+    { 
+      if (letterMap.has(letter)) 
+      { 
+        letterMap.set(letter, letterMap.get(letter) + 1); 
+      } 
+      else { 
+        letterMap.set(letter, 1); 
+      } 
     }
-    return total
+
+    const groupedProducts = Array.from(letterMap.entries()).map(([letter, count]) => { const filteredProducts = productsorders.filter(p => p.Letter === letter); return { letter, count, products: filteredProducts }; });
+    for (const p of groupedProducts) {
+        total += parseInt(p.products[0].Component_Price) * p.count
+    }
+    return {total,groupedProducts}
 }
